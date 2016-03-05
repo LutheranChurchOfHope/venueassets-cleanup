03baca65d676a199c3c4375e2c7f920800737b0b
cf8geek <cf8geek@users.noreply.github.com>
2016-03-05 11:07:26 -0600

    Added files via upload
    first!!1

---
+++ filemaker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+touch workingdir/file1.txt
+touch workingdir/file2.txt
+touch workingdir/2015-09-11_something.txt
+touch workingdir/2016-11-19other.txt
+touch workingdir/2016-11-17_someone.txt
+
+mkdir workingdir/file1.txt2
+mkdir workingdir/file2.txt2
+mkdir workingdir/2015-09-11_something
+mkdir workingdir/2016-11-19other.txt2
+mkdir workingdir/2016-11-17_someone
+
+touch workingdir/2016-11-17_someone/file.txt
+touch workingdir/2015-09-11_something/2016-11-11_someone.txt
+
+mkdir workingdir/20154-11-11_test
+mkdir workingdir/2015-010-01_test
+mkdir workingdir/2017-01-99_test
+mkdir workingdir/2017-10-100_test
+mkdir workingdir/somefolder2017-10-10_test
