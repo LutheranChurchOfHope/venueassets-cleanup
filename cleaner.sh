4c4c283831b9649b31b01934531682339d966281
cf8geek <cf8geek@users.noreply.github.com>
2016-05-30 17:15:10 -0500

    Update cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -9,11 +9,18 @@ function doCleanup() {
   # remove everything (that's left) that doesn't follow the proper dating/naming format
 #  rm -r `ls -1tr ./ | grep -v -e ^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]_ | sed s/$/\'/g | sed s/^/\'/g`
   rm /tmp/myfile.txt
-  ls -1tr ./ | grep -v -e ^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]_ | sed s/\'/\\\\\'/g | sed s/$/\'/g | sed s/^/\'/g >> /tmp/myfile.txt
-  cat /tmp/myfile.txt | sed s/^/rm\ \-r\ /g >> /tmp/myfile2.txt
+  ls -1tr ./ | grep -v -e ^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]_ >> /tmp/myfile.txt
+  #sed s/\'/\\\\\'/g
+  cat /tmp/myfile.txt | sed s/$/\'/g | sed s/^/\'/g >> /tmp/myfile1.txt
+  cat /tmp/myfile1.txt | sed s/^/rm\ \-r\ /g >> /tmp/myfile2.txt
   bash /tmp/myfile2.txt
+  rm /tmp/myfile1.txt
   rm /tmp/myfile2.txt
 
+}
+
+function removeAposTwo() {
+  for i in *; do mv "$i" "$(echo "$i" | tr -d "'")"; done
 }
 
 function removeApos() {
